cb189cc358e40022d715f27c1ca71dd0724498fb
Matt <ammons@gmail.com>
2017-03-28 10:50:50 -0400

    add log class

---
+++ CURL500Test/Log.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CURL400Test
+{
+    class Log
+    {
+        public static string localPath  = "C:\\CURL400\\Logs\\" + DateTime.Now.ToString("yyyyMMdd") + "--Log.txt";
+        public static string networkPath = @"\\nordevengr01\userapps\apps\CURL400\Logs\" + DateTime.Now.ToString("yyyyMMdd") + "--Log.txt";
+
+        public static void permaLog(string sessionInfo, string text)
+        {
+            System.IO.File.AppendAllText(localPath,sessionInfo + " " + text + Environment.NewLine);
+
+            try
+            {
+                System.IO.File.AppendAllText(networkPath, sessionInfo + " " + text + Environment.NewLine);
+            }
+            catch(Exception)
+            {
+                System.IO.File.AppendAllText(localPath, "Couldn't reach network log" + Environment.NewLine);
+            }
+        }
+    }
+}
