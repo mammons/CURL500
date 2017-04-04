2c58b8b7ac5a81086ed9bea0a3050f0d9f32f88f
Matt <ammons@gmail.com>
2017-04-04 17:35:09 -0400

    added PEComArgs

---
+++ CURL500Test/PECommunicationEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CURL500Test
+{
+    public class PECommunicationEventArgs : EventArgs
+    {
+        private string _response;
+
+        public PECommunicationEventArgs(string response)
+        {
+            _response = response;
+        }
+
+        public string response
+        {
+            get { return _response; }
+        }
+    }
+}
