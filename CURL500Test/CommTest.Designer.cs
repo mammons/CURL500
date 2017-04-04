a8f3a4a2c2495341b1351dee2bae81e7b9b67701
Matt <ammons@gmail.com>
2017-04-04 15:34:05 -0400

    bring in main

---
+++ CURL500Test/CommTest.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace CURL500Test
+{
+    partial class CommTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb = new System.Windows.Forms.TextBox();
+            this.command = new System.Windows.Forms.TextBox();
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tb
+            // 
+            this.tb.Location = new System.Drawing.Point(13, 13);
+            this.tb.Multiline = true;
+            this.tb.Name = "tb";
+            this.tb.Size = new System.Drawing.Size(360, 257);
+            this.tb.TabIndex = 0;
+            // 
+            // command
+            // 
+            this.command.Location = new System.Drawing.Point(13, 292);
+            this.command.Name = "command";
+            this.command.Size = new System.Drawing.Size(100, 20);
+            this.command.TabIndex = 1;
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.Location = new System.Drawing.Point(13, 343);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(75, 23);
+            this.sendBtn.TabIndex = 2;
+            this.sendBtn.Text = "Send";
+            this.sendBtn.UseVisualStyleBackColor = true;
+            this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click);
+            // 
+            // CommTest
+            // 
+            this.AcceptButton = this.sendBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 435);
+            this.Controls.Add(this.sendBtn);
+            this.Controls.Add(this.command);
+            this.Controls.Add(this.tb);
+            this.Name = "CommTest";
+            this.Text = "CommTest";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb;
+        private System.Windows.Forms.TextBox command;
+        private System.Windows.Forms.Button sendBtn;
+    }
+}
